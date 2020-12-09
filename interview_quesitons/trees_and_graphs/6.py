7c1161858a7940e3a2da97eeb6748846977ffbb4
funnydman <crazy-dzmitry@mail.ru>
2020-12-09 21:10:43 +0300

    remove unneeded stuff

--- interview_quesitons/trees_and_graphs/6.py
+++ interview_quesitons/trees_and_graphs/6.py
@@ -4,13 +4,4 @@ Successor: Write an algorithm to find the "next" node (i.e., in-order successor)
 binary search tree. You may assume that each node has a link to its parent.
 """
 
-class SuccessorNode:
-    def __int__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-        self.successor = None
-
-
-
 
