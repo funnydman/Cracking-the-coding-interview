1c3755b9d5dec7e3f456858612e9e76ac8b98419
funnydman <crazydzmitry@gmail.com>
2021-06-21 19:47:40 +0300

    Update 4.py

--- interview_quesitons/arrays_and_strings/4.py
+++ interview_quesitons/arrays_and_strings/4.py
@@ -8,12 +8,46 @@ Input: Tact Coa
 Output: True (permutations: "taco cat". "atco cta". etc.)
 """
 
-
-
 def is_perm_of_palindrome(string):
     string = string.strip().lower().replace(" ", "")
     l = len(string)
     return len(set(string))==l//2 if len(string) % 2 == 0  else len(set(string))==(l//2 +1)
 
+def is_perm_of_palindrome1(string):
+    string = string.strip().lower().replace(" ", "")
+    count_odd = 0
+    adict = {}
+
+    for c in string:
+        if c not in adict:
+            adict[c] = 0
+        adict[c] += 1
+
+        if adict[c] % 2 == 1:
+            count_odd +=1
+        else:
+            count_odd -=1
+    return count_odd <= 1
+
+
+def toggle_bit(num, pos):
+    if num & (1 << pos) != 0:
+        return num ^ (1 << pos)
+    return num | (1 << pos)
+
+
+def is_perm_of_palindrome(string):
+    string = string.strip().lower().replace(" ", "")
+    bit_vector = 0
+    for c in string:
+        pos = ord(c) - ord("a")
+        bit_vector = toggle_bit(bit_vector, pos)
+    return bit_vector == 0 or (bit_vector & (bit_vector - 1)) == 0
+
+
 
 assert is_perm_of_palindrome("Tact Coa") is True
+assert is_perm_of_palindrome("abcba") is True
+assert is_perm_of_palindrome("hello") is False
+
+
