1d650e038f780390ab2815ca8566234c0d39a3e4
funnydman <crazydzmitry@gmail.com>
2022-06-19 15:39:30 +0200

    Update 5.py

--- interview_quesitons/stacks_and_queues/5.py
+++ interview_quesitons/stacks_and_queues/5.py
@@ -7,5 +7,18 @@ while stack:
     while tmp_stack and tmp_stack[-1] > temp:
         stack.append(tmp_stack.pop())
     tmp_stack.append(temp)
+    
+    
+def sort_stack(stack):
+    result = []
+    while stack:
+        elem = stack.pop()
+        temp = []
+        while result and elem >= result[-1]:
+            temp.append(result.pop())
+        result.append(elem)
+        while temp:
+            result.append(temp.pop())
+    return result
 
-print(tmp_stack)
+print(sort_stack(stack))
